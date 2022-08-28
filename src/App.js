c49ec89e069f8391991a9dabc1694a2a1f719b36
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-29 00:04:57 +0900

    #31 setState and Secondary callback 3.1

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
                                         lastName: "Neaogie"
                                     }
                                 }
-                            }, ()=> {console.log(this.state)})
+                            })
 
                         }}
 
