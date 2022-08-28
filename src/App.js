f091bbd6d4086720608b5a7e928d1775d4208ace
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-29 01:21:28 +0900

    #40 Searching and filtering 3

--- src/App.js
+++ src/App.js
@@ -36,10 +36,11 @@ class App extends Component {
                     placeholder="search monsters"
                     onChange={(event) => {
                         console.log(event.target.value)
+                        const searchString = event.target.value.toLowerCase()
                         // nếu tên của monster bao gồm chuỗi tìm kiếm, thì giữ nó
                         // nếu nó không bao gồm nó, loại bỏ nó
                         const filteredMonsters = this.state.monsters.filter((monster) => {
-                            return monster.name.includes(event.target.value)
+                            return monster.name.includes(searchString)
                         })
                         this.setState(() => {
                             return {
