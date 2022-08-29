432b96b23d4151cf8715a036b030a2f14df55365
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-30 01:52:19 +0900

    #50 CardList component 1.1

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,8 +7,8 @@ class CardList extends Component {
         return (
             <div className="card-list">
                 {
-                    monsters.map((monster) => {
-                        return (
+                    monsters.map((monster) =>
+                        (
                             <div className="card-container" key={monster.id}>
                                 <img
                                     src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
@@ -17,9 +17,8 @@ class CardList extends Component {
                                 <h2>{monster.name}</h2>
                                 <p>{monster.email}</p>
                             </div>
-                            // <h1 key={monster.id}>{monster.name}</h1>
                         )
-                    })
+                    )
                 }
             </div>
         );
