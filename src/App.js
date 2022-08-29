a8a54ee0839fd0eb774481f88f49edec987819f9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-30 00:59:08 +0900

    #46 Component Props 1

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
                 {/*        )*/}
                 {/*    })*/}
                 {/*}*/}
-                <CardList/>
+                <CardList monsters={"I am the monsters"}/>
             </div>
         );
     }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class CardList extends Component {
     render() {
+        console.log(this.props)
         return (
             <div>
                 CardList Component
