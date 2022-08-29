6d5b0f6d130ec5117b3e4bece1a5f431f63b509c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-30 02:05:02 +0900

    #52 Finishing Touches 2

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import "./card.styles.css"
 
 class Card extends Component {
     render() {
+        const {id, name, email} = this.props.monster;
         return (
-            <div>
-
+            <div className="card-container" key={id}>
+                <img
+                    src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                    alt={`monster ${name}`}
+                />
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         );
     }
