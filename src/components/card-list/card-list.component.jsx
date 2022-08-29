f0b3999311bb0aff4c704e386cbe6763609452eb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-30 01:50:27 +0900

    #50 CardList component 1

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,11 +5,19 @@ class CardList extends Component {
         console.log("Render")
         const {monsters} = this.props;
         return (
-            <div>
+            <div className="card-list">
                 {
                     monsters.map((monster) => {
                         return (
-                            <h1 key={monster.id}>{monster.name}</h1>
+                            <div className="card-container" key={monster.id}>
+                                <img
+                                    src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                                    alt={`monster ${monster.name}`}
+                                />
+                                <h2>{monster.name}</h2>
+                                <p>{monster.email}</p>
+                            </div>
+                            // <h1 key={monster.id}>{monster.name}</h1>
                         )
                     })
                 }
