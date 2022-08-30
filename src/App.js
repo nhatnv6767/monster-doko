0766af6ff9b46e0eac23207c02097590cc05b4c4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-30 21:41:50 +0900

    #53 Functional components 1

--- src/App.js
+++ src/App.js
@@ -3,65 +3,81 @@ import "./App.css"
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
 
-    // always first
-    constructor(props) {
-        super(props);
-        this.state = {
-            monsters: [],
-            searchField: ""
-        }
-    }
-
-    // 3rd
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response =>
-                response.json()
-            )
-            .then((users) => this.setState(() => {
-                return {
-                    monsters: users
-                }
-            }, () => {
-                // console.log(this.state)
-            }))
-    }
-
-    onSearchChange = (event) => {
-        const searchField = event.target.value.toLocaleLowerCase()
-
-        this.setState(() => {
-            return {
-                searchField
-            }
-        })
-    }
-
-    // 2nd
-    render() {
-
-        const {monsters, searchField} = this.state;
-        const {onSearchChange} = this
-        // nếu tên của monster bao gồm chuỗi tìm kiếm, thì giữ nó
-        // nếu nó không bao gồm nó, loại bỏ nó
-        const filteredMonsters = monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchField)
-        })
-
-        return (
-            <div className="App">
-                <h1 className="app-title">Monsters Rolodex</h1>
-                <SearchBox
-                    onChangeHandler={onSearchChange}
-                    placeholder="search monsters"
-                    className="monsters-search-box"
-                />
-                <CardList monsters={filteredMonsters}/>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="app-title">Monsters Rolodex</h1>
+            {/*<SearchBox*/}
+            {/*    onChangeHandler={onSearchChange}*/}
+            {/*    placeholder="search monsters"*/}
+            {/*    className="monsters-search-box"*/}
+            {/*/>*/}
+            {/*<CardList monsters={filteredMonsters}/>*/}
+        </div>
+    )
 }
+
+
+// class App extends Component {
+//
+//     // always first
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             monsters: [],
+//             searchField: ""
+//         }
+//     }
+//
+//     // 3rd
+//     componentDidMount() {
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//             .then(response =>
+//                 response.json()
+//             )
+//             .then((users) => this.setState(() => {
+//                 return {
+//                     monsters: users
+//                 }
+//             }, () => {
+//                 // console.log(this.state)
+//             }))
+//     }
+//
+//     onSearchChange = (event) => {
+//         const searchField = event.target.value.toLocaleLowerCase()
+//
+//         this.setState(() => {
+//             return {
+//                 searchField
+//             }
+//         })
+//     }
+//
+//     // 2nd
+//     render() {
+//
+//         const {monsters, searchField} = this.state;
+//         const {onSearchChange} = this
+//         // nếu tên của monster bao gồm chuỗi tìm kiếm, thì giữ nó
+//         // nếu nó không bao gồm nó, loại bỏ nó
+//         const filteredMonsters = monsters.filter((monster) => {
+//             return monster.name.toLocaleLowerCase().includes(searchField)
+//         })
+//
+//         return (
+//             <div className="App">
+//                 <h1 className="app-title">Monsters Rolodex</h1>
+//                 <SearchBox
+//                     onChangeHandler={onSearchChange}
+//                     placeholder="search monsters"
+//                     className="monsters-search-box"
+//                 />
+//                 <CardList monsters={filteredMonsters}/>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
