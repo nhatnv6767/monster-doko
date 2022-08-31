69d7b05e9fcd00eab75f7a4a2ffaea9a7018ad68
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-31 21:55:49 +0900

    #59 Infinite re-rendering 5 explain

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
 
     console.log("render")
 
+    // giải thích bên dưới
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(response =>
             response.json()
@@ -104,3 +105,20 @@ const App = () => {
 // }
 
 export default App;
+
+/*
+Trường hợp này sẽ xảy ra lặp vô hạn, lý do:
+Fetch đang tìm nạp từ API bên ngoài, một số giá trị cụ thể
+trong trường hợp của chúng ta đó là mảng users
+với mảng này, vì nó đến từ bên ngoài trình duyệt, nên nó sẽ cung cấp cho chúng ta
+một mảng có bộ nhớ khác với mảng mà chúng ta đã lưu trữ
+Ngay cả khi các giá trị bên trong mảng này hoàn toàn giống nhau (như ta biết thì api này
+luôn trả về 10 users giống nhau mỗi khi ta gọi API)
+Nhưng vì nó đến từ bên ngoài trình duyệt, nó là 1 mảng khác trong bộ nhớ
+Mỗi lần check happpen (setMonster(users)) nó sẽ thấy răng mảng của user này là 1 mảng khác
+trong bộ nhớ. Vì vậy, việc kiểm tra bình đẳng không thành công, có nghĩa là React nghĩ rằng
+nó sẽ là 1 mảng khác. Đó là 1 mảng khác, ngay cả khi các giá trị bên trong giống nhau.
+Hãy nhớ là nó lúc này không lin quan đến các giá trị trong mảng.
+Đó là việc mảng đó có trỏ đến cùng 1 tham chiếu trong bộ nhớ hay không
+Và mỗi khi điều này xảy ra, nó là 1 mảng khác trong bộ nhớ
+* */
