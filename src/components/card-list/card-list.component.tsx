5847d682d5ec2c9cffb19b7085e93d8474db93d7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-30 21:48:00 +0900

    #196 Typing React's setState 4

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,7 +1,12 @@
 import "./card-list.styles.css"
 import Card from "../card/card.component";
+import {Monster} from "../../App";
 
-const CardList = ({monsters}) => {
+type CardListProps = {
+    monsters: Monster[],
+}
+
+const CardList = ({monsters}: CardListProps) => {
     return (
         <div className="card-list">
             {
