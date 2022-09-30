679ebf3af47bd62abe252dbf8cc31d356673985f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-30 21:43:00 +0900

    #196 Typing React's setState 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
-import {useEffect, useState} from "react";
+import {useEffect, useState, ChangeEvent} from "react";
 import {getData} from "./utils/data.utils";
 
 type Monster = {
@@ -19,17 +19,11 @@ const App = () => {
     console.log("rendered")
 
     useEffect(() => {
-        // fetch("https://jsonplaceholder.typicode.com/users")
-        //     .then(response =>
-        //         response.json()
-        //     )
-        //     .then((users) =>
-        //         setMonsters(users)
-        //     )
         const fetchUsers = async () => {
             const users = await getData<Monster[]>("https://jsonplaceholder.typicode.com/users")
             setMonsters(users)
         }
+        fetchUsers();
     }, [])
 
     useEffect(() => {
@@ -40,7 +34,7 @@ const App = () => {
     }, [monsters, searchField])
 
 
-    const onSearchChange = (event) => {
+    const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
         const searchFieldString = event.target.value.toLocaleLowerCase()
         setSearchField(searchFieldString)
     }
